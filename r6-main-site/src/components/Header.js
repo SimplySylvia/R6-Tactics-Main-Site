044bc54d4ec49e064dda28cb72cdf3181274a6c0
DaltonHart <daltonvaren@gmail.com>
2018-06-26 13:52:14 -0700

    updated materialize added styling and routes

---
+++ r6-main-site/src/components/Header.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class Header extends Component{
+  render(){
+    return (
+      <header>
+        <div className="navbar-fixed">
+        <nav>
+          <div className="nav-wrapper">
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li><Link to={'/'}>Home</Link></li>
+              <li>Ops</li>
+            </ul>
+          </div>
+        </nav>
+        </div>
+      </header>
+    )
+  }
+}
+
+export default Header
