fafb7e8b473f8ee4f307c61ae37307002b5a4e53
DaltonHart <daltonvaren@gmail.com>
2018-07-05 14:32:20 -0700

    added sign up and sign in along with styling

--- r6-main-site/src/components/fixedHeader.js
+++ r6-main-site/src/components/fixedHeader.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Container,Dropdown,Image,Menu,} from 'semantic-ui-react'
+import {Container,Dropdown,Image,Menu,Button, MenuItem} from 'semantic-ui-react'
 import {Link} from 'react-router-dom';
 
 const FixedMenuLayout = () => (
@@ -7,7 +7,7 @@ const FixedMenuLayout = () => (
     <Menu fixed='top' inverted>
       <Container>
         <Menu.Item as='a' header>
-          <Image size='mini' src='/logo.png' style={{ marginRight: '1.5em' }} />
+          <Image size='mini' src='./images/logo.png' style={{ marginRight: '1.5em' }} />
           R6 Tactics
         </Menu.Item>
         <Menu.Item as='a'><Link to={'/'}>Home</Link></Menu.Item>
@@ -15,20 +15,24 @@ const FixedMenuLayout = () => (
         <Dropdown item simple text='Menu'>
           <Dropdown.Menu>
             <Dropdown.Item>Strats</Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
+            <Dropdown.Item>Forum</Dropdown.Item>
             <Dropdown.Divider />
-            <Dropdown.Header>Header Item</Dropdown.Header>
+            <Dropdown.Header>Game Details</Dropdown.Header>
             <Dropdown.Item>
               <i className='dropdown icon' />
               <span className='text'>Operators</span>
               <Dropdown.Menu>
-                <Dropdown.Item><Link to={'/AttackOps'}>Attack Operators</Link></Dropdown.Item>
-                <Dropdown.Item><Link to={'/DefenseOps'}>Defense Operators</Link></Dropdown.Item>
+                <Dropdown.Item ><Link className="subMenu" to={'/AttackOps'}>Attack Operators</Link></Dropdown.Item>
+                <Dropdown.Item ><Link className="subMenu" to={'/DefenseOps'}>Defense Operators</Link></Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown.Item>
             <Dropdown.Item>Maps</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
+        <Menu.Item position='right'>
+        <Button as='a' primary> Sign In </Button>
+        <Button as='a' primary> Sign Up </Button>
+        </Menu.Item>
       </Container>
     </Menu>
   </div>
