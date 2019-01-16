c785f8441849ea532567542076e2fede3307dd03
Dalton Hart <DaltonVaren@gmail.com>
2019-01-15 16:35:03 -0800

    added auth verification to app

--- r6-main-site/src/App.js
+++ r6-main-site/src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import MyRoutes from './config/routes';
 import FixedMenuLayout from './components/fixedHeader'
+import {AUTH} from './config/constants'
+import axios from 'axios'
 
 class App extends Component {
   state={
     verified: false
   }
   componentDidMount () {
+    let token = localStorage.token
     if (localStorage.token) {
-      this.setState({
-        verified: true
+      console.log('tokenfound', token)
+      axios({ method: 'post', url: `${AUTH}/verify`, headers: { authorization: `Bearer ${token}` } })
+      .then((response)=>{
+        console.log(response)
+        this.setState({
+          verified: true,
+          userId: response.data.id
+        })
       })
+        
     } else {
       this.setState({
         verified: false
@@ -18,10 +28,20 @@ class App extends Component {
     }
   }
 
+  handleLogOut = () => {
+    this.setState({
+      verified: false
+    })
+    localStorage.clear()
+  }
+
   render() {
     return (
       <div className="App">
-      <FixedMenuLayout verified={this.state.verified}/>
+      <FixedMenuLayout 
+          verified={this.state.verified}
+          handleLogOut={this.handleLogOut}
+        />
         { MyRoutes }
       </div>
     );
